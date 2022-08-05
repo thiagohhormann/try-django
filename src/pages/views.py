3afbe74edb1327825a75f63ef0f023271fd0f4b1
thiagohhormann <thiago.h.hormann.gmail.com>
2022-08-05 10:06:49 -0300

    Url routing and request/response

--- src/pages/views.py
+++ src/pages/views.py
@@ -3,4 +3,14 @@ from django.shortcuts import render
 
 # Create your views here.
 def home_view(*args, **kwargs):
+    # print(request.user)
     return HttpResponse("<h1>Hello World</h1>") #string of HTML code
+
+def contact_view(request, *args, **kwargs):
+    return HttpResponse("<h1>Contact Page</h1>")
+
+def contact_view(request, *args, **kwargs):
+    return HttpResponse("<h1>Contact Page</h1>")
+
+def contact_view(request, *args, **kwargs):
+    return HttpResponse("<h1>Contact Page</h1>")
