e8ad03adcc1e585378c74c2d9d77f04a707a7b9e
thiagohhormann <thiago.h.hormann.gmail.com>
2022-08-11 10:29:22 -0300

    add a context and context variable to my pages

--- src/pages/views.py
+++ src/pages/views.py
@@ -11,7 +11,12 @@ def contact_view(request, *args, **kwargs):
     return render(request, "contact.html", {})
 
 def about_view(request, *args, **kwargs):
-    return render(request, "about.html", {})
+    my_context = {
+        "my_text":"This is about us, my context.",
+        "my_number": 123,
+        "my_list:" [122, 154, 64544],
+    }
+    return render(request, "about.html", my_context)
 
 def social_view(request, *args, **kwargs):
     return render(request, "socials.html", {})
